6dc1f39e0399f90923cc809250d0ce9434516370
一只咸鱼的大厂梦 <11516488+hxw6666666@user.noreply.gitee.com>
2025-05-31 20:46:47 +0800

    leecode_42_接雨水

---
+++ algorithm/src/Hot100/double_point/leecode_42_接雨水.java
@@ -0,0 +1,46 @@
+package Hot100.double_point;
+
+import java.sql.PreparedStatement;
+
+/**
+ * 没有思路
+ *
+ * @author hxw
+ * @date 2025-05-31 20:22
+ */
+public class leecode_42_接雨水 {
+    public static void main(String[] args) {
+        int[] height = {4,2,0,3,2,5};
+        System.out.println(trap(height));
+    }
+    public static int trap(int[] height){
+        // 定义两个指针，指向数组的左右两边
+        int left = 0;
+        int right = height.length - 1;
+        // 记录左右两边的最大高度
+        int leftMax = 0;
+        int rightMax = 0;
+
+        // 记录结果
+        int result = 0;
+        // 移动较小的一边
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] > leftMax){
+                    leftMax = height[left];
+                }else{
+                    result += leftMax - height[left];
+                }
+                left ++;
+            }else{
+                if(height[right] > rightMax){
+                    rightMax = height[right];
+                }else{
+                    result += rightMax - height[right];
+                }
+                right --;
+            }
+        }
+        return result;
+    }
+}
