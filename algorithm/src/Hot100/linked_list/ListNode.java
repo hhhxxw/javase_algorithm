3045af0d4d7509884f3a686748a207786f94c375
一只咸鱼的大厂梦 <11516488+hxw6666666@user.noreply.gitee.com>
2025-06-17 23:23:32 +0800

    leecode_234_回文链表

---
+++ algorithm/src/Hot100/linked_list/ListNode.java
@@ -0,0 +1,24 @@
+package Hot100.linked_list;
+
+/**
+ * @author 一只咸鱼的大厂梦-hxw
+ * @date 2025-06-17 23:21
+ */
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ algorithm/src/Hot100/linked_list/leecode_234_回文链表.java
@@ -0,0 +1,25 @@
+package Hot100.linked_list;
+
+public class leecode_234_回文链表 {
+    public static void main(String[] args) {
+
+    }
+    public boolean isPalindrome(ListNode head) {
+        int[] arr = new int[100010];
+        int size = 0;
+        while(head != null){
+            arr[size ++] = head.val;
+            head = head.next;
+        }
+        int i = 0, j = size - 1;
+        while(i < j){
+            if(arr[i] == arr[j]){
+                i ++;
+                j --;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
