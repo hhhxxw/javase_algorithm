1a143355e99e876486d0da317f6374aac7aefcfc
一只咸鱼的大厂梦 <11516488+hxw6666666@user.noreply.gitee.com>
2025-06-21 22:09:59 +0800

    leecode_21_合并两个有序链表

---
+++ algorithm/src/Hot100/linked_list/leecode_21_合并两个有序链表.java
@@ -0,0 +1,35 @@
+package Hot100.linked_list;
+
+/**
+ * @author 一只咸鱼的大厂梦-hxw
+ * @date 2025-06-21 22:08
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class leecode_21_合并两个有序链表 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+    }
+}
